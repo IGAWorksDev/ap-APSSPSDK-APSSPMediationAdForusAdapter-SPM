b4b41a7f57796513991a31884fc4c9a764bcbdba
HwanghoSong <odin.song@adpopcorn.com>
2026-09-01 10:07:06 +0900

    13010000.0.0(13.1.0.0 Adapter)배포

--- Sources/MediationAdForus/AdForusAdapterFactory.swift
+++ Sources/MediationAdForus/AdForusAdapterFactory.swift
@@ -6,7 +6,7 @@ public final class AdForusAdapterFactory: NSObject, APSSPAdapterFactory {
 
     public static var networkID: Int { 28 }
     public static var sdkVersion: String? { AdForusInitializationAdapter().sdkVersion }
-    public static var adapterVersion: String? { "13.0.0.0" }
+    public static var adapterVersion: String? { "13.1.0.0" }
 
     public static func makeInitializationAdapter() -> AnyObject? {
         return AdForusInitializationAdapter()
